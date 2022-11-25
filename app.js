5d22ff32e39b3c7ed21168e4520bc24bfe632b02
kingbowang <2508357595@qq.com>
2022-11-25 16:10:28 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+// const CONNECTION_STRING = 'mongodb+srv://pengbo:<password>@cluster0.sttowde.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const schema = mongoose.Schema({
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    image: String,
+    username: String,
+    handle: String,
+    time: String,
+}, {collection: 'tuits'});
+export default schema;
